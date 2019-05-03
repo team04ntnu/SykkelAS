2ce3b70d289f8ff812a0a452740367c048cd67f6
OleJ <ole.joachim@hotmail.com>
2019-05-03 10:48:43 +0200

    change statistics form

--- SykkelAS/SykkelAS/Statistikk.Designer.vb
+++ SykkelAS/SykkelAS/Statistikk.Designer.vb
@@ -22,9 +22,103 @@ Partial Class Statistikk
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.btnBeregn = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.dtpDatoFra = New System.Windows.Forms.DateTimePicker()
+        Me.dtpDatoTil = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'btnBeregn
+        '
+        Me.btnBeregn.Location = New System.Drawing.Point(125, 246)
+        Me.btnBeregn.Name = "btnBeregn"
+        Me.btnBeregn.Size = New System.Drawing.Size(164, 23)
+        Me.btnBeregn.TabIndex = 0
+        Me.btnBeregn.Text = "Fremstill Statistikk"
+        Me.btnBeregn.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(365, 82)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(365, 106)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(365, 130)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(547, 82)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(208, 173)
+        Me.ListBox1.TabIndex = 4
+        '
+        'dtpDatoFra
+        '
+        Me.dtpDatoFra.Location = New System.Drawing.Point(58, 78)
+        Me.dtpDatoFra.Name = "dtpDatoFra"
+        Me.dtpDatoFra.Size = New System.Drawing.Size(200, 20)
+        Me.dtpDatoFra.TabIndex = 5
+        '
+        'dtpDatoTil
+        '
+        Me.dtpDatoTil.Location = New System.Drawing.Point(58, 121)
+        Me.dtpDatoTil.Name = "dtpDatoTil"
+        Me.dtpDatoTil.Size = New System.Drawing.Size(200, 20)
+        Me.dtpDatoTil.TabIndex = 6
+        '
+        'Statistikk
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dtpDatoTil)
+        Me.Controls.Add(Me.dtpDatoFra)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.btnBeregn)
+        Me.Name = "Statistikk"
         Me.Text = "Statistikk"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents btnBeregn As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents dtpDatoFra As DateTimePicker
+    Friend WithEvents dtpDatoTil As DateTimePicker
 End Class
--- SykkelAS/SykkelAS/Statistikk.vb
+++ SykkelAS/SykkelAS/Statistikk.vb
@@ -1,3 +1,37 @@
 ﻿Public Class Statistikk
+    Dim dato_fra, dato_til As Date
+    Dim fra, til As String
+    Private Sub btnBeregn_Click(sender As Object, e As EventArgs) Handles btnBeregn.Click
+        dato_fra = dtpDatoFra.Value
+        dato_til = dtpDatoTil.Value
+        fra = dato_fra.ToString("yyyy-MM-dd")
+        til = dato_til.ToString("yyyy-MM-dd")
 
+        MsgBox(til & fra)
+        'Dim cnn3 As New SqlConnection
+        'Dim cmd3 As New SqlCommand
+
+        'cnn3.ConnectionString = ("Data Source=SARAHSCOMPUTER;Initial Catalog=FYPDB1;Integrated Security=True")
+        'cmd3.Connection = cnn3
+
+        'Dim tblFields As String = "SELECT COUNT(Location) AS LocationCount, Location AS LocationName FROM tblTagInfo group by Location"
+        'Dim oData As New SqlDataAdapter(tblFields, cnn3)
+        'Dim ds As New DataSet
+        'Dim oCmd As New SqlCommand(tblFields, cnn3)
+
+
+        'cnn3.Open()
+        'oData.Fill(ds, "tblTagInfo")
+        'cnn3.Close()
+
+        'Chart1.DataSource = ds.Tables("tblTagInfo")
+        'Dim Series1 As Series = Chart1.Series("Series1")
+        'Series1.Name = "Sales"
+        'Chart1.Series(Series1.Name).XValueMember = "LocationName"
+        'Chart1.Series(Series1.Name).YValueMembers = "LocationCount"
+
+        'Chart1.Size = New System.Drawing.Size(780, 350
+
+
+    End Sub
 End Class
