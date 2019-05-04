b5d98a354e4a5de95ab495ecced1d133909a3cce
OleJAasen <ole.joachim@hotmail.com>
2019-05-04 23:34:01 +0200

    add feature statistics

--- SykkelAS/SykkelAS/Statistikk.Designer.vb
+++ SykkelAS/SykkelAS/Statistikk.Designer.vb
@@ -26,14 +26,24 @@ Partial Class Statistikk
         Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
         Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.btnBeregn = New System.Windows.Forms.Button()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.rbtnSykkel = New System.Windows.Forms.RadioButton()
+        Me.rbtnAvdeling = New System.Windows.Forms.RadioButton()
+        Me.rbtnAvanse = New System.Windows.Forms.RadioButton()
+        Me.lstboxSykkelLokasjon = New System.Windows.Forms.ListBox()
         Me.dtpDatoFra = New System.Windows.Forms.DateTimePicker()
         Me.dtpDatoTil = New System.Windows.Forms.DateTimePicker()
         Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.txtLønn = New System.Windows.Forms.TextBox()
+        Me.txtFasteUtgifter = New System.Windows.Forms.TextBox()
+        Me.txtVariableUtgifter = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSykkelLokasjon = New System.Windows.Forms.Button()
+        Me.btnHovedmeny = New System.Windows.Forms.Button()
         CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnBeregn
@@ -45,46 +55,46 @@ Partial Class Statistikk
         Me.btnBeregn.Text = "Fremstill Statistikk"
         Me.btnBeregn.UseVisualStyleBackColor = True
         '
-        'RadioButton1
+        'rbtnSykkel
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(282, 74)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(90, 17)
-        Me.RadioButton1.TabIndex = 1
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "RadioButton1"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.rbtnSykkel.AutoSize = True
+        Me.rbtnSykkel.Location = New System.Drawing.Point(282, 74)
+        Me.rbtnSykkel.Name = "rbtnSykkel"
+        Me.rbtnSykkel.Size = New System.Drawing.Size(68, 17)
+        Me.rbtnSykkel.TabIndex = 1
+        Me.rbtnSykkel.TabStop = True
+        Me.rbtnSykkel.Text = "Pr sykkel"
+        Me.rbtnSykkel.UseVisualStyleBackColor = True
         '
-        'RadioButton2
+        'rbtnAvdeling
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(282, 98)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(90, 17)
-        Me.RadioButton2.TabIndex = 2
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "RadioButton2"
-        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.rbtnAvdeling.AutoSize = True
+        Me.rbtnAvdeling.Location = New System.Drawing.Point(282, 98)
+        Me.rbtnAvdeling.Name = "rbtnAvdeling"
+        Me.rbtnAvdeling.Size = New System.Drawing.Size(78, 17)
+        Me.rbtnAvdeling.TabIndex = 2
+        Me.rbtnAvdeling.TabStop = True
+        Me.rbtnAvdeling.Text = "Pr avdeling"
+        Me.rbtnAvdeling.UseVisualStyleBackColor = True
         '
-        'RadioButton3
+        'rbtnAvanse
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(282, 122)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(90, 17)
-        Me.RadioButton3.TabIndex = 3
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "RadioButton3"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.rbtnAvanse.AutoSize = True
+        Me.rbtnAvanse.Location = New System.Drawing.Point(282, 122)
+        Me.rbtnAvanse.Name = "rbtnAvanse"
+        Me.rbtnAvanse.Size = New System.Drawing.Size(61, 17)
+        Me.rbtnAvanse.TabIndex = 3
+        Me.rbtnAvanse.TabStop = True
+        Me.rbtnAvanse.Text = "Avanse"
+        Me.rbtnAvanse.UseVisualStyleBackColor = True
         '
-        'ListBox1
+        'lstboxSykkelLokasjon
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(547, 12)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(208, 173)
-        Me.ListBox1.TabIndex = 4
+        Me.lstboxSykkelLokasjon.FormattingEnabled = True
+        Me.lstboxSykkelLokasjon.Location = New System.Drawing.Point(547, 12)
+        Me.lstboxSykkelLokasjon.Name = "lstboxSykkelLokasjon"
+        Me.lstboxSykkelLokasjon.Size = New System.Drawing.Size(208, 173)
+        Me.lstboxSykkelLokasjon.TabIndex = 4
         '
         'dtpDatoFra
         '
@@ -106,7 +116,7 @@ Partial Class Statistikk
         Me.Chart1.ChartAreas.Add(ChartArea1)
         Legend1.Name = "Legend1"
         Me.Chart1.Legends.Add(Legend1)
-        Me.Chart1.Location = New System.Drawing.Point(497, 227)
+        Me.Chart1.Location = New System.Drawing.Point(468, 215)
         Me.Chart1.Name = "Chart1"
         Series1.ChartArea = "ChartArea1"
         Series1.Legend = "Legend1"
@@ -116,33 +126,129 @@ Partial Class Statistikk
         Me.Chart1.TabIndex = 7
         Me.Chart1.Text = "Chart1"
         '
+        'txtLønn
+        '
+        Me.txtLønn.Location = New System.Drawing.Point(112, 39)
+        Me.txtLønn.Name = "txtLønn"
+        Me.txtLønn.Size = New System.Drawing.Size(100, 20)
+        Me.txtLønn.TabIndex = 8
+        '
+        'txtFasteUtgifter
+        '
+        Me.txtFasteUtgifter.Location = New System.Drawing.Point(112, 65)
+        Me.txtFasteUtgifter.Name = "txtFasteUtgifter"
+        Me.txtFasteUtgifter.Size = New System.Drawing.Size(100, 20)
+        Me.txtFasteUtgifter.TabIndex = 9
+        '
+        'txtVariableUtgifter
+        '
+        Me.txtVariableUtgifter.Location = New System.Drawing.Point(112, 91)
+        Me.txtVariableUtgifter.Name = "txtVariableUtgifter"
+        Me.txtVariableUtgifter.Size = New System.Drawing.Size(100, 20)
+        Me.txtVariableUtgifter.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Lønn"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Faste utgifter"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Variable utgifter"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtVariableUtgifter)
+        Me.GroupBox1.Controls.Add(Me.txtLønn)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtFasteUtgifter)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(211, 215)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(251, 170)
+        Me.GroupBox1.TabIndex = 16
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Utgifter"
+        Me.GroupBox1.Visible = False
+        '
+        'btnSykkelLokasjon
+        '
+        Me.btnSykkelLokasjon.Location = New System.Drawing.Point(770, 162)
+        Me.btnSykkelLokasjon.Name = "btnSykkelLokasjon"
+        Me.btnSykkelLokasjon.Size = New System.Drawing.Size(154, 23)
+        Me.btnSykkelLokasjon.TabIndex = 17
+        Me.btnSykkelLokasjon.Text = "Lokasjon av Sykkel"
+        Me.btnSykkelLokasjon.UseVisualStyleBackColor = True
+        '
+        'btnHovedmeny
+        '
+        Me.btnHovedmeny.Location = New System.Drawing.Point(812, 542)
+        Me.btnHovedmeny.Name = "btnHovedmeny"
+        Me.btnHovedmeny.Size = New System.Drawing.Size(157, 23)
+        Me.btnHovedmeny.TabIndex = 18
+        Me.btnHovedmeny.Text = "Hovedmeny"
+        Me.btnHovedmeny.UseVisualStyleBackColor = True
+        '
         'Statistikk
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(981, 577)
+        Me.Controls.Add(Me.btnHovedmeny)
+        Me.Controls.Add(Me.btnSykkelLokasjon)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Chart1)
         Me.Controls.Add(Me.dtpDatoTil)
         Me.Controls.Add(Me.dtpDatoFra)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.RadioButton3)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.lstboxSykkelLokasjon)
+        Me.Controls.Add(Me.rbtnAvanse)
+        Me.Controls.Add(Me.rbtnAvdeling)
+        Me.Controls.Add(Me.rbtnSykkel)
         Me.Controls.Add(Me.btnBeregn)
         Me.Name = "Statistikk"
         Me.Text = "Statistikk"
         CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnBeregn As Button
-    Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
-    Friend WithEvents RadioButton3 As RadioButton
-    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents rbtnSykkel As RadioButton
+    Friend WithEvents rbtnAvdeling As RadioButton
+    Friend WithEvents rbtnAvanse As RadioButton
+    Friend WithEvents lstboxSykkelLokasjon As ListBox
     Friend WithEvents dtpDatoFra As DateTimePicker
     Friend WithEvents dtpDatoTil As DateTimePicker
     Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents txtLønn As TextBox
+    Friend WithEvents txtFasteUtgifter As TextBox
+    Friend WithEvents txtVariableUtgifter As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSykkelLokasjon As Button
+    Friend WithEvents btnHovedmeny As Button
 End Class
--- SykkelAS/SykkelAS/Statistikk.vb
+++ SykkelAS/SykkelAS/Statistikk.vb
@@ -4,49 +4,106 @@ Public Class Statistikk
     Dim dato_fra, dato_til As Date
     Dim fra, til As String
     Dim statistikktabell As New DataTable
+    Dim lønn, fasteUtgifter, variableUtgifter As Integer
+
+    Private Sub btnHovedmeny_Click(sender As Object, e As EventArgs) Handles btnHovedmeny.Click
+        Me.Hide()
+        Hovedmeny.Show()
+    End Sub
+
+    Private Sub rbtnAvanse_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnAvanse.CheckedChanged
+        GroupBox1.Visible = True
+    End Sub
+
+    Dim nr As Integer
 
     Private Sub btnBeregn_Click(sender As Object, e As EventArgs) Handles btnBeregn.Click
         dato_fra = dtpDatoFra.Value
         dato_til = dtpDatoTil.Value
         fra = dato_fra.ToString("yyyy-MM-dd")
         til = dato_til.ToString("yyyy-MM-dd")
+        nr = Innlogging.innloggetAvdeling.HentAvdelingNr()
 
+        MsgBox(fra & vbNewLine & til & vbNewLine & nr)
 
         Try
             databasetilkobling.databaseTilkobling()
             tilkobling.Open()
 
-            Dim sporring As String = "SELECT sykkel_id, COUNT(*) FROM utleid_sykkel INNER JOIN leieavtale ON utleid_sykkel.leieavtale_nr = leieavtale.leieavtale_nr WHERE leieavtale.tidspunkt_fra BETWEEN '2019-05-02' AND '2019-05-08' GROUP BY sykkel_id"
-            Dim sql As New MySqlCommand(sporring, tilkobling)
-            Dim da As New MySqlDataAdapter
-            da.SelectCommand = sql
-            da.Fill(statistikktabell)
+            If rbtnSykkel.Checked = True Then
 
-            tilkobling.Close()
+                Dim sql As New MySqlCommand("SELECT sykkel_id, COUNT(*) FROM utleid_sykkel INNER JOIN leieavtale ON utleid_sykkel.leieavtale_nr = leieavtale.leieavtale_nr WHERE leieavtale.avdeling_nr = @nr AND leieavtale.tidspunkt_fra BETWEEN '@fra' AND '@til' GROUP BY sykkel_id", tilkobling)
+                sql.Parameters.AddWithValue("@fra", fra)
+                sql.Parameters.AddWithValue("@til", til)
+                sql.Parameters.AddWithValue("@nr", nr)
+                Dim leser = sql.ExecuteReader()
+                'Dim da As New MySqlDataAdapter
+                'da.SelectCommand = sql
+                'da.Fill(statistikktabell)
+                statistikktabell.Load(leser)
+                tilkobling.Close()
+                Me.Controls.Add(Chart1)
+                Dim Series1 As New Series()
+                Chart1.Name = "Chart1"
+                Series1.ChartArea = "ChartArea1"
+                Series1.Legend = "Legend1"
+                Series1.Name = "Series1"
+                Chart1.Size = New System.Drawing.Size(500, 200)
+                Chart1.TabIndex = 0
+                Chart1.Text = "Chart1"
+                Chart1.Series("Series1").XValueMember = "Sykkel_id"
+                Chart1.Series("Series1").YValueMembers = "COUNT(*)"
+                Chart1.DataSource = statistikktabell.Rows
 
+            ElseIf rbtnAvdeling.Checked = True Then
 
+                Dim sql2 As New MySqlCommand("SELECT COUNT(*) FROM utleid_sykkel INNER JOIN leieavtale ON utleid_sykkel.leieavtale_nr = leieavtale.leieavtale_nr WHERE leieavtale.avdeling_nr = @nr AND leieavtale.tidspunkt_fra BETWEEN '@fra' AND '@til'", tilkobling)
+                sql2.Parameters.AddWithValue("@fra", fra)
+                sql2.Parameters.AddWithValue("@til", til)
+                sql2.Parameters.AddWithValue("@avdeling", nr)
+                sql2.ExecuteNonQuery()
+                Dim da As New MySqlDataAdapter
+                da.SelectCommand = sql2
+                da.Fill(statistikktabell)
+                tilkobling.Close()
+                Me.Controls.Add(Chart1)
+                Dim Series1 As New Series()
+                Chart1.Name = "Chart1"
+                Series1.ChartArea = "ChartArea1"
+                Series1.Legend = "Legend1"
+                Series1.Name = "Series1"
+                Chart1.Size = New System.Drawing.Size(500, 200)
+                Chart1.TabIndex = 0
+                Chart1.Text = "Chart1"
+                Chart1.Series("Series1").XValueMember = "Sykkel_id"
+                Chart1.Series("Series1").YValueMembers = "COUNT(*)"
+                Chart1.DataSource = statistikktabell.Rows
 
+            ElseIf rbtnAvanse.Checked = True Then
 
+                Dim sql3 As New MySqlCommand("SELECT sykkel_id, COUNT(*) FROM utleid_sykkel INNER JOIN leieavtale ON utleid_sykkel.leieavtale_nr = leieavtale.leieavtale_nr WHERE leieavtale.avdeling_nr = @nr AND leieavtale.tidspunkt_fra BETWEEN '@fra' AND '@til' GROUP BY sykkel_id", tilkobling)
+                sql3.Parameters.AddWithValue("@fra", fra)
+                sql3.Parameters.AddWithValue("@til", til)
+                sql3.Parameters.AddWithValue("@avdeling", nr)
+                sql3.ExecuteNonQuery()
+                Dim da As New MySqlDataAdapter
+                da.SelectCommand = sql3
+                da.Fill(statistikktabell)
+                tilkobling.Close()
+                Me.Controls.Add(Chart1)
+                Dim Series1 As New Series()
+                Chart1.Name = "Chart1"
+                Series1.ChartArea = "ChartArea1"
+                Series1.Legend = "Legend1"
+                Series1.Name = "Series1"
+                Chart1.Size = New System.Drawing.Size(500, 200)
+                Chart1.TabIndex = 0
+                Chart1.Text = "Chart1"
+                Chart1.Series("Series1").XValueMember = "Sykkel_id"
+                Chart1.Series("Series1").YValueMembers = "COUNT(*)"
+                Chart1.DataSource = statistikktabell.Rows
 
-            Me.Controls.Add(Chart1)
-            Dim Series1 As New Series()
-
-
-
-            Chart1.Name = "Chart1"
-            Series1.ChartArea = "ChartArea1"
-            Series1.Legend = "Legend1"
-            Series1.Name = "Series1"
-            Chart1.Size = New System.Drawing.Size(500, 200)
-            Chart1.TabIndex = 0
-            Chart1.Text = "Chart1"
-
-
-            Chart1.Series("Series1").XValueMember = "Sykkel_id"
-            Chart1.Series("Series1").YValueMembers = "COUNT(*)"
-
-            Chart1.DataSource = statistikktabell.Rows
-
+            End If
 
         Catch feilmelding As Exception
             MsgBox(feilmelding.Message)
